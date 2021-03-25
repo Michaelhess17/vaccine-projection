d394aaa33e7fc4f3ab295982a5130c7d1e4ed642
Michael Hess <michaelhess17@yahoo.com>
2021-03-25 18:19:02 -0400

    nightly update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd covid-vaccine-tracker-data
-git pull origin master
+wget -O data/historical_usa_doses_administered.csv https://raw.githubusercontent.com/BloombergGraphics/covid-vaccine-tracker-data/master/data/historical-usa-doses-administered.csv 
 cd ..
 wget -O data/us_state_vaccinations.csv https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv
 git add .
