b0299b906508eeb537bf80e316bce66c557e2047
Michael Hess <michaelhess17@yahoo.com>
2021-03-27 18:04:42 -0400

    fix updater

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ cd covid-vaccine-tracker-data
 wget -O data/historical_usa_doses_administered.csv https://raw.githubusercontent.com/BloombergGraphics/covid-vaccine-tracker-data/master/data/historical-usa-doses-administered.csv 
 cd ..
 wget -O data/us_state_vaccinations.csv https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv
-python3 -m calc_coefs.py
+python3 calc_coefs.py
 now=$(date)
 echo $now
